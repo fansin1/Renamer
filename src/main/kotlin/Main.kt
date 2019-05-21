14187370ac4b10423562ebe3244cabfddc342732
FanSin <artem21481@gmail.com>
2019-05-21 16:32:26 +0300

    Release 1.0 All works

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,26 @@
+import java.io.File
+
 object Main {
 
     @JvmStatic
     fun main(args: Array<String>) {
-        
+        if (args.isEmpty()) {
+            println("No arguments")
+            return
+        }
+        File(args[0]).walk().forEach {
+            if (it.name.endsWith(".java") || it.name.endsWith(".kt")) {
+
+                val newFile = File(it.path + ".2019")
+                if (newFile.exists()) {
+                    println("File " + it.path + " already exists")
+                } else if (!it.renameTo(newFile)) {
+                    println(it.path + " can't be renamed")
+                } else {
+                    println(it.path)
+                }
+            }
+        }
     }
 
 }
